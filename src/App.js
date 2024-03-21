9f8cde51e1fd902e22df64d0cc137874b5875c3c
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-21 15:37:59 +0200

    redux-toolkit and redux-persist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="dark">
-      <h1 className="text-red-500">WhatsApp</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
