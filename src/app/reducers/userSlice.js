2c571ce08875656723ee8a26d4d481df6288a5e7
MOSTAFA837 <mostafamanchester837@gmail.com>
2024-03-22 16:25:11 +0200

    register a user

--- src/app/reducers/userSlice.js
+++ src/app/reducers/userSlice.js
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const AUTH_ENDPOINT = `${process.env.REACT_APP_API_ENDPOINT}/auth`;
 
 const initialState = {
   status: "",
@@ -12,6 +15,21 @@ const initialState = {
     token: "",
   },
 };
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (values, { rejectedWithValue }) => {
+    try {
+      const { data } = await axios.post(`${AUTH_ENDPOINT}/register`, {
+        ...values,
+      });
+
+      return data;
+    } catch (error) {
+      return rejectedWithValue(error.response.data.error.message);
+    }
+  }
+);
 
 export const userSlice = createSlice({
   name: "user",
@@ -29,8 +47,25 @@ export const userSlice = createSlice({
         token: "",
       };
     },
+    changeStatus: (state, action) => {
+      state.status = action.payload;
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(registerUser.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.user = action.payload.user;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
   },
 });
 
-export const { logout } = userSlice.actions;
+export const { logout, changeStatus } = userSlice.actions;
 export default userSlice.reducer;
